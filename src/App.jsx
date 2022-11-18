fe776a5b08cbf90569b85a11d6b06234c178245c
Maxim <334890@gmail.com>
2022-11-18 21:07:21 +1000

    v01

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+//import logo from "./logo.svg";
+import "./fontawesome/css/fontawesome.css";
+import "./fontawesome/css/solid.css";
+import "./fontawesome/css/brands.css";
+import React from "react";
+
+function App() {
+  const getRandomColor = () => {
+    const i = Math.floor(Math.random() * 10);
+    const palette = [
+      "#e74c3c",
+      "#73a857",
+      "#f39c12",
+      "#9b59b6",
+      "#fb6964",
+      "#2c3e50",
+      "#16a085",
+      "#27ae60",
+      "#f39c12",
+      "#342224",
+    ];
+    return palette[i];
+  };
+
+  const newQuote = () => {
+    document.documentElement.style.setProperty(
+      "--randomColor",
+      getRandomColor()
+    );
+    const j = Math.floor(Math.random() * 100);
+    setCurrentQuote(quotesArray[j]);
+  };
+
+  const [quotesArray, setQuotesArray] = React.useState([]);
+  const [currentQuote, setCurrentQuote] = React.useState({
+    quote: "loading...",
+    author: "loading...",
+  });
+
+  React.useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data.quotes);
+        setQuotesArray(data.quotes);
+      });
+    newQuote();
+  }, []);
+
+  return (
+    <div className="app">
+      <div id="wrapper">
+        <div id="content">
+          <p id="quote">
+            <i className="fa-solid fa-quote-left" id="quotemark"></i>
+            {currentQuote.quote}
+          </p>
+          <p id="author">- {currentQuote.author}</p>
+          <div id="btn-container">
+            <button className="btn btn-icon">
+              <i className="fa-brands fa-twitter"></i>
+            </button>
+            <button className="btn btn-icon">
+              <i className="fa-brands fa-tumblr"></i>
+            </button>
+            <button className="btn btn-new-q" onClick={newQuote}>
+              New quote
+            </button>
+          </div>
+        </div>
+        <div id="footer">
+          <p id="link">the link</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
